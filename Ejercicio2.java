990c3e33db89fd512ae4e4b9fbabcd1ab90df182
AitorArcas890 <aar890@educa.madrid.org>
2025-12-22 19:33:28 +0100

    Mis Problemas Resueltos

---
+++ Ejercicio2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Introduce el numero de horas");
+        int horas= sc.nextInt();
+        int dias= horas /24;
+        int horasr = horas %24;
+        System.out.println(horas + "El desglose es: " + dias + " dias y " + horas + " horas ");
+
+    }
+}
